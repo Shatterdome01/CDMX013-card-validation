f14ffac7131af733ebc5e1e0634cbd972d2a77eb
Shatterdome01 <108882333+Shatterdome01@users.noreply.github.com>
2022-08-03 20:19:36 -0500

    Cambio sin read me

--- src/index.js
+++ src/index.js
@@ -1,3 +1,79 @@
 import validator from './validator.js';
 
+let validation = document.getElementById('btnValidator');
+let cardNumber = document.getElementById('cardNumber');
+let ccv = document.getElementById('ccvText');
+let expirationDate = document.getElementById('date');
+let showMessage = document.getElementById('validationMessage');
+
+//boton de validar
+//validation.addEventListener('click', validateForm()
+    // function(){
+    //     let valueCN = cardNumber.value;
+    //     let maskifyValue = validator.maskify(valueCN);
+    //     document.getElementById('cardNumber').value = maskifyValue;
+    //     console.log(validator.isValid(valueCN));
+    //     console.log(validator.maskify(valueCN));
+    // }
+//);
+
+//Solo ingrese números
+cardNumber.addEventListener('keypress', function (e) {
+    if (!onlyNumbers(event)) {
+        e.preventDefault();
+    }
+});
+function onlyNumbers(e) {
+    let key = e.charCode;
+    //console.log(key);
+    return key >= 48 && key <= 57;
+}
+
+function validateForm() {
+    let dataCCV = ccv.value;
+    let dataDate = expirationDate.value;
+    let valueCN = cardNumber.value;
+    if (valueCN === ''){
+        return alert('ingresa algo, credit card number');
+    } else {
+        let maskifyValue = validator.maskify(valueCN);
+         document.getElementById('cardNumber').value = maskifyValue;
+
+         let validation = validator.isValid(valueCN);
+
+         if (validation == true){
+            showMessage.innerHTML = 'Transaction Aproved';
+
+         } else {
+            showMessage.innerHTML = 'Transaction denied';
+         }
+
+    if (dataCCV === ''){
+        return alert('ingresa un número de cuenta');
+    }
+
+    if (dataDate === ''){
+        return alert('ingresa algo, date');
+    }
+
+    
+    }
+
+}
+validation.addEventListener('click', validateForm);
+
+// function validateForm(){
+//     if(valueCN ==''){
+//         return alert('Ingresa un número de tarjeta');
+//     } if (valueCV < 16){
+//         return alert('Ingresa los 16 números válidos');
+//     } 
+
+
+// }
+
+
+
 console.log(validator);
+
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,54 @@
 const validator = {
-  // ...
-};
+  isValid: function (valueCN) {
+    let output = valueCN.split('').reverse(); // con esta linea separo el numero y lo pongo al revés
+    let arrayReverse;
+    //***** Aquí pongo el número en un arreglo y lo pongo al revés.
+    let valueCardNumber = [];
+    for (let i = 0; i < output.length; i++) {
+      valueCardNumber.push(parseInt(output[i]));
+    }
+    arrayReverse = valueCardNumber;
+    //*****Aqui agrega a un nuevo Array los puros numeros encontrados en posiciones pares y multiplicarlos x2
+    let arrayPairs = [];
+    for (let i = 1; i < arrayReverse.length; i = i + 2) {
+      arrayPairs.push(arrayReverse[i] * 2);
+    }
+    //*****Aqui agrego a un nuevo arreglo los numeros con 2 digitos y se suman.
+    let arraySumPairs = [];
+    for (let i = 0; i < arrayPairs.length; i++) {
+      if (arrayPairs[i] > 9) {
+        arraySumPairs.push(arrayPairs[i] - 9);//suma los digitos de aquellos numeros con 2 digitos.
+      }
+    }
+    //*****Aqui filtro los resultados de 2 digitos de un par y los concateno con los anteriores en posiciones pares.
+    let filterDigists = arrayPairs.filter(number => number < 10);
+    let arrayPairsFinally = arraySumPairs.concat(filterDigists);
+    //console.log(arrayPairsFinally);
 
+    //*****Aquí concatenaré los pares con los demás números y los sumaré
+    let oddArray = arrayReverse.filter((number, index) => index % 2 == 0);
+    let totalArray = oddArray.concat(arrayPairsFinally);
+
+    //suma de todos los digitos
+    let sum = 0;
+    for (let i = 0; i < totalArray.length; i++) {
+      sum = sum + totalArray[i];
+    }
+    if (sum % 10 == 0) {
+      return true;
+    } else {
+      return false;
+    }
+  },
+  maskify: function (valueCN) {
+    if (valueCN.length > 4) {
+      let sustitute = "#".repeat(valueCN.length - 4);
+      let last = valueCN.substring(valueCN.length - 4);
+      let outputMask = sustitute + last;
+      return outputMask;
+    } else {
+      return valueCN;
+    }
+  }
+};
 export default validator;
